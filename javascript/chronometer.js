cf70d523c8a0aa8c308cb4b2198a8ca75e35b5d8
AidaG91 <aida.garmus@gmail.com>
2025-07-03 13:45:42 +0200

    Interation 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+
+      if (typeof callback === 'function') {
+        callback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let stringValue = String(value);
+    let paddedString = '00' + stringValue;
+    return paddedString.slice(-2);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.getMinutes();
+    const seconds = this.getSeconds();
+
+    const formattedMinutes = this.computeTwoDigitNumber(minutes);
+    const formattedSeconds = this.computeTwoDigitNumber(seconds);
+
+    return `${minutesFormat}:${secondsFormat}`;
   }
 }
 
